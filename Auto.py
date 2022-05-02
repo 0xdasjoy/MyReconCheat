a08c4931bea2cdcaae434c1c6761e2606ebd72bd
r00x <41233127+xpentester@users.noreply.github.com>
2022-05-02 13:09:43 +0600

    Update and rename Auto.html to Auto.py

--- Auto.py
+++ Auto.py
@@ -13,3 +13,18 @@ crlf scan -i "urls.txt"
 
 XSRFProbe
 The Prime Cross Site Request Forgery (CSRF) Audit and Exploitation Toolkit.
+python xsrfprobe.py -u http://example.com --crawl
+python3 xsrfprobe.py -u http://example.com/testcsrf.php -c "SESSION_ID=i837c5n83u4e84cry9e4, _gid=jdhfbuysfcefjbhiiwer"
+
+
+Autorize
+
+burp
+
+Oralyzer
+Public
+Open Redirection Analyzer
+
+python3 Oralyzer.py -u list.txt
+
+
